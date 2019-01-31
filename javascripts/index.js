9d8daba3a2cda9603f7b4fd32c5e823586f0180d
chenkailei <ckl5975@126.com>
2019-01-31 20:32:25 +0800

    轮播图

---
+++ javascripts/index.js
@@ -0,0 +1,22 @@
+// 搜索框
+// 搜索框
+
+
+// banner图
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: true,//可选选项，自动滑动
+    effect : 'fade',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+    pagination: {
+    el: '.swiper-pagination',
+    clickable :true
+    }
+})
+
+// banner图
+
+// 大家都在说
+
